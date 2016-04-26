663199d88db888141ec5798ee2322ed4e8cf5b53
Klafyvel <klafyvel@gmail.com>
2016-04-26 17:40:47 +0200

    Documentation de System, Component et documentation succinte de Application.

--- include/KlafEngine/Core.hpp
+++ include/KlafEngine/Core.hpp
@@ -29,33 +29,60 @@ namespace klf
 {
 	class Application;
 	class System;
+
+	/** @class Component
+	 * @brief A component has to store datas.
+	 */
 	class Component
 	{
 	public:
+		/** @brief Constructor.
+		 * @param e The entity which own the component.
+		 */
 		Component(unsigned int e) : entity(e){}
+
+		/** @brief Dynamically allow a Component for the given entity.
+		 * @param entity The entity which own the component.
+		 */
 		static std::unique_ptr<Component> createEmptyComponent(unsigned int entity)
 		{
 			std::unique_ptr<Component> c(new Component(entity));
 			return c;
 		}
-		unsigned int entity;
-		std::unordered_map<std::string, int> int_values;
-		std::unordered_map<std::string, std::string> string_values;
-		std::unordered_map<std::string, float> float_values;
+
+		unsigned int entity; /** The component's owner.*/
+		std::unordered_map<std::string, int> int_values; /** Every int value stored in the component. */
+		std::unordered_map<std::string, std::string> string_values; /** Every std::string value stored in the component. */
+		std::unordered_map<std::string, float> float_values; /** Every float value stored in the component. */
 	};
 
+	/** @brief Typedef for component factories.
+	 */
 	typedef std::function<std::unique_ptr<Component>(unsigned int)> EmptyComponentFactory;
 
+	/** @class System
+	 * @brief Deal with Component's values.
+	 */
 	class System
 	{
 	public:
+		/** @brief Constructor.
+		 * @param application A reference to the system owner.
+		 */
 		System(Application &application) : m_application(application){}
 	protected:
+		/** @brief Access to the entity's component. May not be overrided
+		 * @param mask The component's mask.
+		 * @param entity The entity.
+		 */
 		Component& getComponent(const ComponentMask mask, const unsigned int entity);
 
-		Application& m_application;
+		Application& m_application;/** System owner.*/
 	};
 
+	/** @class Application
+	 * @brief Handle every system, components and entities.
+	 */
 	class Application
 	{
 	friend class System;
