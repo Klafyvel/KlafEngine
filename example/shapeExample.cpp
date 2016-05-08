78ef05561d6219a8dc6746b3a6863cc25f3cb4a9
Klafyvel <klafyvel@gmail.com>
2016-05-08 11:55:43 +0200

    Ouuuh un exemple sans bug c'est cool.

--- example/shapeExample.cpp
+++ example/shapeExample.cpp
@@ -3,7 +3,7 @@
 #include <SFML/Graphics.hpp>
 
 #include <KlafEngine/Core.hpp>
-#include <KlafEngine/ComponentMask.hpp>
+#include <KlafEngine/ComponentsMask.hpp>
 #include <KlafEngine/Renderer.hpp>
 #include <KlafEngine/Shape.hpp>
 
@@ -12,6 +12,29 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(800, 600), "KlafEngine");
 	klf::Application app;
 
-	app.registerComponentType(SHAPE, Shape::shapeFactory);
+	app.registerComponentType(klf::SHAPE, klf::Shape::shapeFactory);
+
+	klf::ShapesHandler s(app);
+	klf::Renderer r(app);
+	klf::Entity e = s.createShape();
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		r.render(window);
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
+}
+
+
 
 
